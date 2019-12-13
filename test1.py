1df3cf08559aec30e1bda3e516f00cfdb9ae26a5
pascal <pascal@Pascals-Laptop.local>
2019-12-13 12:22:24 -0800

    documentation

--- test1.py
+++ test1.py
@@ -3,24 +3,24 @@ import time
 import numpy as np
 
 
+# dont touch
 def half(tup):
     return int(tup[1] / 2), int(tup[0] / 2)
 
 
+# complicated
 def generate_pix(img):
     x_tol = 3
     y_tol = 3
-    for xval in (x_tol, int(img.shape[1]/2), img.shape[1] - x_tol):
-        for yval in (y_tol, int(img.shape[0]/2), img.shape[0] - y_tol):
+    for xval in (x_tol, int(img.shape[1] / 2), img.shape[1] - x_tol):
+        for yval in (y_tol, int(img.shape[0] / 2), img.shape[0] - y_tol):
             try:
                 yield img[xval, yval]
             except IndexError:
                 pass
-        # for _ in range(1, img.shape[1] - 8):
-        #    yield img[xval, _]
-    # for top in range(5)
 
 
+# what do you think it does?
 def touching_wall(img):
     for _ in generate_pix(img):
         if not np.array_equal(_, np.asarray([0, 0, 0, 255])):
@@ -28,19 +28,24 @@ def touching_wall(img):
     return False
 
 
+# read images from disk
 backdrop = cv2.imread("resources/images/move map.png")
 backdrop = cv2.cvtColor(backdrop, cv2.COLOR_RGB2RGBA)
 pac = cv2.imread("resources/images/pacman0.png")
 pac = cv2.cvtColor(pac, cv2.COLOR_RGB2RGBA)
 
+# resize images
 backdrop = cv2.resize(backdrop, half(backdrop.shape))
 pac = cv2.resize(pac, half(pac.shape))
 
-print(backdrop.shape)
-fps_target = 144
-speed = 1
+# print(backdrop.shape)
+# initialize vars
 backy, backx, channels = backdrop.shape
 pacy, pacx, channels = pac.shape
+
+## GLOBAL SETTINGS ##
+fps_target = 144
+speed = 1
 direction = 180
 x = int(backx / 2 - pacx / 2)
 y = 290
@@ -49,9 +54,11 @@ del channels
 
 while True:
 
+    # frame setup
     start_time = time.time()
     frame = backdrop.copy()
 
+    # read keys
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
@@ -59,14 +66,16 @@ while True:
         direction = 90
     elif key == ord('s') and not touching_wall(frame[y + tolerance:pacy + y + tolerance, x:pacx + x]):
         direction = 270
-    elif key == ord('a') and not pacx + x - tolerance < 0 and not touching_wall(frame[y:pacy + y, x - tolerance:pacx + x - tolerance]):
+    elif key == ord('a') and not pacx + x - tolerance < 0 and not touching_wall(
+            frame[y:pacy + y, x - tolerance:pacx + x - tolerance]):
         direction = 180
-    elif key == ord('d') and not pacx + x + tolerance > backx and not touching_wall(frame[y:pacy + y, x + tolerance:pacx + x + tolerance]):
+    elif key == ord('d') and not pacx + x + tolerance > backx and not touching_wall(
+            frame[y:pacy + y, x + tolerance:pacx + x + tolerance]):
         direction = 0
     else:
         pass
-        # print("dumbass")
 
+    # actually move pacman
     if direction == 0 and not touching_wall(frame[y:pacy + y, x + speed:pacx + x + speed]):
         x += speed
     elif direction == 90 and not touching_wall(frame[y - speed:pacy + y - speed, x:pacx + x]):
@@ -77,23 +86,22 @@ while True:
         y += speed
     else:
         pass
-        # raise Exception("i bet nico messed it up")
 
+    # make sure he stays on the map
     x = max(x, 0)
     y = max(y, 0)
     x = min(x, backx - pacx)
     y = min(y, backy - pacy)
-    # print(x, backx, pacx)
 
-    # print(touching_wall(frame[y:pacy + y, x:pacx + x]))
     frame[y:pacy + y, x:pacx + x] = pac
-    # cv2.rectangle(frame, (x, y), (x + pacx, y + pacy), (0, 255, 0), 1)
+    # cv2.rectangle(frame, (x, y), (x + pacx, y + pacy), (0, 255, 0), 1) # enable this to draw the bounding box
 
+    # fps cap
     while (1.0 / (time.time() - start_time)) > (fps_target + 0.02):
         time.sleep(0.000001)
 
+    # draw the fps and then show the frame
     frame = cv2.putText(frame, f'FPS: {round(1.0 / (time.time() - start_time))}', (0, 15),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
 
     cv2.imshow("roman i want to die", frame)
-
