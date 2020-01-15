c15b42a9ca77a1844b0501cdb3f6c60ef5e55234
cheeser3832 <cheeser3832@gmail.com>
2020-01-15 12:04:17 -0800

    git commit

--- test1.py
+++ test1.py
@@ -56,7 +56,9 @@ def touching_wall(img):
 
 
 # read images from disk
-backdrop = cv2.imread("resources/images/move map.png")
+movemap = cv2.imread("resources/images/move map.png")
+movemap = cv2.cvtColor(movemap, cv2.COLOR_RGB2RGBA)
+backdrop = cv2.imread("resources/images/backdrop.png")
 backdrop = cv2.cvtColor(backdrop, cv2.COLOR_RGB2RGBA)
 pacs = [cv2.cvtColor(cv2.imread("./resources/images/pacman0.png", cv2.IMREAD_UNCHANGED), cv2.COLOR_RGB2RGBA),
         cv2.cvtColor(cv2.imread("./resources/images/pacman1.png", cv2.IMREAD_UNCHANGED), cv2.COLOR_RGB2RGBA),
@@ -64,8 +66,9 @@ pacs = [cv2.cvtColor(cv2.imread("./resources/images/pacman0.png", cv2.IMREAD_UNC
         cv2.cvtColor(cv2.imread("./resources/images/pacman1.png", cv2.IMREAD_UNCHANGED), cv2.COLOR_RGB2RGBA)]
 
 # resize images
-backdrop = cv2.resize(backdrop, (480, 640))# half(backdrop.shape))
-print(half(backdrop.shape))
+movemap = cv2.resize(movemap, (480, 640))# half(backdrop.shape))
+backdrop = cv2.resize(backdrop, (480, 640)) #810,1080
+#print(half(movemap.shape))
 for pac in range(len(pacs)):
     pacs[pac] = cv2.resize(pacs[pac], half(pacs[pac].shape))
 
@@ -88,33 +91,35 @@ for i in range(len(pac)):
 # cv2.imshow("test", np.zeros((30, 30, 4)))
 # print(backdrop.shape)
 # initialize vars
-backy, backx, channels = backdrop.shape
+backy, backx, channels = movemap.shape
 pacy, pacx, channels = pac.shape
 
 ## GLOBAL SETTINGS ##
 fps_target = 144
 speed = 1
-direction = 180
+direction = 90
 x = int(backx / 2 - pacx / 2)
-y = 290
+y = 349
+#597
 tolerance = 10
 ghost_settings = (("Blinky", (0, 0)), ("Clyde", (0, 50)), ("Inky", (50, 0)), ("Pinky", (50, 50)))
 del channels
 
 ghosts = [Ghost(i[0], i[1]) for i in ghost_settings]
-print(ghosts)
+#print(ghosts)
 next_pac_frame = next_pac_frame()
 
 while True:
 
     # frame setup
     start_time = time.time() - 0.00001
-    frame = backdrop.copy()
+    frame = movemap.copy()
+    frame1 = backdrop.copy()
     pac_local = next(next_pac_frame)
 
     # read keys
     key = cv2.waitKey(1)
-    print(key)
+    #print(key)
     if key == ord('q'):
         break
     elif key == ord('w') and not touching_wall(frame[y - tolerance:pacy + y - tolerance, x:pacx + x]):
@@ -129,6 +134,9 @@ while True:
         direction = 0
     else:
         pass
+    
+    #if cv2.getWindowProperty("roman",0) != 0:
+    #    break
 
     # actually move pacman
     if direction == 0 and not touching_wall(frame[y:pacy + y, x + speed:pacx + x + speed]):
@@ -157,8 +165,8 @@ while True:
     # frame[y:pacy + y, x:pacx + x] = pac_local
     pac_show = np.zeros((backy, backx, 4), dtype='uint8')  # np.array([[[0, 0, 0, 255]] * backx] * backy, dtype='uint8')
     pac_show[y:pacy + y, x:pacx + x] = pac_local
-    frame = cv2.addWeighted(frame, 1.0, pac_show, 10.0, 10)
-    # cv2.rectangle(frame, (x, y), (x + pacx, y + pacy), (0, 255, 0), 1) # enable this to draw the bounding box
+    frame1 = cv2.addWeighted(frame1, 1.0, pac_show, 10.0, 10)
+    #cv2.rectangle(frame, (x, y), (x + pacx, y + pacy), (0, 255, 0), 1) # enable this to draw the bounding box
 
     # fps cap
 
@@ -166,7 +174,8 @@ while True:
         time.sleep(0.000001)
 
     # draw the fps and then show the frame
-    frame = cv2.putText(frame, 'FPS: {}'.format(round(1.0 / (time.time() - start_time))), (0, 15),
+    frame1 = cv2.putText(frame1, 'FPS: {}'.format(round(1.0 / (time.time() - start_time))), (0, 15),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
 
-    cv2.imshow("roman i want to die", frame)
+    cv2.imshow("roman", frame)
+    cv2.imshow("roman", frame1)
