076dad9c7b4fc74148cdaf96dac063c9fb15019a
cheeser3832 <cheeser3832@gmail.com>
2020-01-24 10:53:27 -0800

    123

--- test1-copy.py
+++ test1-copy.py
@@ -91,7 +91,11 @@ def addDots(fra):
             if np.array_equal(dotmap[pely][pelx], np.asarray([0, 0, 0, 255])):
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 fra = cv2.circle(fra, (pelx, pely), 2, (120, 150, 150), -1)
+=======
+                fra = cv2.circle(fra, (pelx, pely), 3, (150, 180, 180), -1)
+>>>>>>> 52755aca04e8e4329f4a55250ef3b157025f6c57
 =======
                 fra = cv2.circle(fra, (pelx, pely), 3, (150, 180, 180), -1)
 >>>>>>> 52755aca04e8e4329f4a55250ef3b157025f6c57
