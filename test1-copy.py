95fc618cec7afd395a5cf0f15a3137c44dc19bbb
cheeser3832 <cheeser3832@gmail.com>
2020-02-06 12:09:38 -0800

    123

--- test1-copy.py
+++ test1-copy.py
@@ -11,7 +11,6 @@ rotate = {
     0: cv2.ROTATE_180
 }
 
-
 class Ghost:
     positions = {  # so that all ghosts dont hit each other
         "Blinky": None,
@@ -24,21 +23,6 @@ class Ghost:
         self.img = name + "img"
         self.name = name
         self.coord = start
-
-    """def move(self, map): bro tyler idk wut u did but this code is not even close to anything resembling something that will execute without error
-    
-        self.positions[self.name] = self.coord  # sync positions
-        if direction == 0 and not touching_wall(map[y:pacy + y, x + speed:pacx + x + speed]):
-            x += speed
-        elif direction == 90 and not touching_wall(map[y - speed:pacy + y - speed, x:pacx + x]):
-            y -= speed
-        elif direction == 180 and not touching_wall(map[y:pacy + y, x - speed:pacx + x - speed]):
-            x -= speed
-        elif direction == 270 and not touching_wall(map[y + speed:pacy + y + speed, x:pacx + x]):
-            y += speed
-        else:
-            pass
-        # do some move action"""
 
     def __repr__(self):
         return self.name + " at " + self.coord.__repr__()
@@ -94,18 +78,24 @@ pinkyimg = [
 dotmap = cv2.imread("resources/images/dotmap.png")
 dotmap = cv2.cvtColor(dotmap, cv2.COLOR_RGB2RGBA)
 
+powerup = cv2.imread("resources/images/powerup.png")
+powerup = cv2.cvtColor(powerup, cv2.COLOR_RGB2RGBA)
+
 # resize images
 movemap = cv2.resize(movemap, (480, 640))  # half(backdrop.shape))
 backdrop = cv2.resize(backdrop, (480, 640))  # 810,1080
 dotmap = cv2.resize(dotmap, (480, 640))
-# print(half(movemap.shape))
+powerup = cv2.resize(powerup, half(powerup.shape))
 
 for pac in range(len(pacs)):
     pacs[pac] = cv2.resize(pacs[pac], half(pacs[pac].shape))
 
 pac = pacs[0]
 pelpos = []
+score = 0
 
+def initPowerUps():
+    
 
 def initDots():
     start = (25, 75)
@@ -124,10 +114,12 @@ def peltest(dotx, doty):
 
 
 def addDots(fra):
+    global score
     for pel in pelpos:
         if pel[2] is 1:
             fra = cv2.circle(fra, (pel[0], pel[1]), 3, (150, 180, 180), -1)
             if peltest(pel[0], pel[1]) is True:
+                score = score + 1
                 pel[2] = 0
         else:
             pass
@@ -140,15 +132,6 @@ def next_pac_frame():
             for i in range(10):
                 yield step.copy()
 
-
-"""
-for i in range(len(pac)):
-    for j in range(len(pac[i])):
-        if np.array_equal(pac[i][j], np.array([0, 0, 0, 0])):
-            pac[i][j] = np.array([0, 0, 0, 0])"""
-
-# cv2.imshow("test", np.zeros((30, 30, 4)))
-# print(backdrop.shape)
 # initialize vars
 backy, backx, channels = movemap.shape
 pacy, pacx, channels = pac.shape
@@ -165,23 +148,21 @@ ghost_settings = (("Blinky", (390, 410)), ("Clyde", (390, 490)), ("Inky", (335, 
 del channels
 
 ghosts = [Ghost(i[0], i[1]) for i in ghost_settings]
-# print(Ghost[1])
 next_pac_frame = next_pac_frame()
 
 initDots()
 
 while True:
-
+    
     # frame setup
     start_time = time.time() - 0.00001
     movemap = movemap.copy()
     frame1 = backdrop.copy()
     pac_local = next(next_pac_frame)
     drawDots = executor.submit(addDots, frame1)
-
+    
     # read keys
     key = cv2.waitKey(1)
-    # print(key)
     if key == ord('q'):
         break
     elif key == ord('w') and not touching_wall(movemap[y - tolerance:pacy + y - tolerance, x:pacx + x]):
@@ -224,15 +205,10 @@ while True:
 
     start = (25, 75)
     end = (465, 585)
-    """
-    for pelx in range(start[0], end[0], 20):
-        for pely in range(start[1], end[1], 19):
-            if np.array_equal(dotmap[pely][pelx], np.asarray([0, 0, 0, 255])):
-                frame1 = cv2.circle(frame1, (pelx, pely), 2, (100, 100, 100))"""
+
 
     pac_local = cv2.rotate(pac_local, rotate[direction]) if direction != 180 else pac_local
-    # frame[y:pacy + y, x:pacx + x] = pac_local
-    pac_show = np.zeros((backy, backx, 4), dtype='uint8')  # np.array([[[0, 0, 0, 255]] * backx] * backy, dtype='uint8')
+    pac_show = np.zeros((backy, backx, 4), dtype='uint8')
     pac_show[y:pacy + y, x:pacx + x] = pac_local
 
     futures.wait([drawDots], return_when=futures.ALL_COMPLETED)
@@ -242,19 +218,11 @@ while True:
 
     # fps cap
 
-    # xs = (25, 60, 110, 160, 215, 265, 315, 365, 415, 450)
-    # ys = (80, 150, 205, 260, 310, 360, 420, 470, 525, 580)
-
-    # print(np.array_equal(frame1, frame))
-
-    # print(drawDots.result())
-
     while (1.0 / (time.time() - start_time)) > (fps_target + 0.02):
         time.sleep(0.000001)
 
     # draw the fps and then show the frame
     frame1 = cv2.putText(frame1, 'FPS: {}'.format(round(1.0 / (time.time() - start_time))), (0, 15),
                          cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-
-    # cv2.imshow("roman", frame)
+    frame1 = cv2.putText(frame1, 'Score: {}'.format(score), (0, 30), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
     cv2.imshow("roman", frame1)
