50cc24043bd7b273dd5c6bdeaf10450f93c91b75
cheese <58524963+cheese622@users.noreply.github.com>
2020-03-26 22:14:10 -0700

    Update test1-copy.py

--- test1-copy.py
+++ test1-copy.py
@@ -11,7 +11,7 @@ rotate = {
     0: cv2.ROTATE_180
 }
 
-
+# initializes ghosts, not fully implemented yet (JP)
 class Ghost:
     positions = {  # so that all ghosts dont hit each other
         "Blinky": None,
@@ -46,7 +46,7 @@ def generate_pix(img):
                 pass
 
 
-# what do you think it does?
+# detects if parameter img in question is touching one of the walls
 def touching_wall(img):
     for _ in generate_pix(img):
         if not np.array_equal(_, np.asarray([0, 0, 0, 255])):
@@ -54,7 +54,7 @@ def touching_wall(img):
     return False
 
 
-# read images from disk
+# read and load images from disk
 movemap = cv2.imread("resources/images/move_map.png")
 movemap = cv2.cvtColor(movemap, cv2.COLOR_RGB2RGBA)
 backdrop = cv2.imread("resources/images/backdrop.png")
@@ -91,12 +91,13 @@ pac = pacs[0]
 pelpos = []
 score = 0
 
+#powerup positions and presets powered up state for pacman
 powerUpPos = [[25, 113, 1], [445, 113, 1], [25, 474, 1], [445, 474, 1]]
 poweredUp = False
 
 startime = 0
 
-
+#adds powerups onto map, tests if pacman hit a powerup, and powers him up (Tyler)
 def addPowerUps(fra):
     global startime
     global poweredUp
@@ -113,7 +114,7 @@ def addPowerUps(fra):
             pass
     return True
 
-
+#initializes positions of all dots on screen into an array (Pascal)
 def initDots():
     start = (25, 75)
     end = (465, 585)
@@ -122,14 +123,14 @@ def initDots():
             if np.array_equal(dotmap[pely][pelx], np.asarray([0, 0, 0, 255])):
                 pelpos.append([pelx, pely, 1])
 
-
+#the test for if pacman ate a dot (Pascal)
 def peltest(dotx, doty):
     if x <= dotx <= x + pac.shape[1] and y <= doty <= y + pac.shape[0]:
         return True
     else:
         return False
 
-
+#adds the dots onto the map and also uses peltest to 'eat' the dots (Tyler)
 def addDots(fra):
     global score
     for pel in pelpos:
@@ -142,7 +143,7 @@ def addDots(fra):
             pass
     return True
 
-
+#animates pacman
 def next_pac_frame():
     while True:
         for step in pacs:
@@ -170,6 +171,7 @@ next_pac_frame = next_pac_frame()
 
 initDots()
 
+#main game loop
 while True:
 
     # frame setup
@@ -208,8 +210,6 @@ while True:
     else:
         pass
 
-    # for ghost
-
     # make sure he stays on the map
     x = max(x, 0)
     y = max(y, 0)
@@ -224,11 +224,8 @@ while True:
     start = (25, 75)
     end = (465, 585)
 
-    # if poweredUp:
-    # do power up code
-    # pass
 
-    print(poweredUp)
+    #print(poweredUp)
 
     pac_local = cv2.rotate(pac_local, rotate[direction]) if direction != 180 else pac_local
     pac_show = np.zeros((backy, backx, 4), dtype='uint8')
@@ -249,5 +246,6 @@ while True:
     # draw the fps and then show the frame
     frame1 = cv2.putText(frame1, 'FPS: {}'.format(round(1.0 / (time.time() - start_time))), (0, 15),
                          cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+    #displays score
     frame1 = cv2.putText(frame1, 'Score: {}'.format(score), (backx-100, 15), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
     cv2.imshow("roman", frame1)
